1ba76243d9c67577f3efa44af3ef710a37419b30
tejas <chaudharitejas771@gmail.com>
2024-03-04 10:48:21 +0530

    fixed engine

--- middleware/error.js
+++ middleware/error.js
@@ -1,18 +1,11 @@
-class ErrorHandler extends Error{
-    constructor(message,statusCode){
-        super(message);
-        this.statusCode= statusCode;
-    }
+const errormiddleware = (err, req, res, next) => {
+    err.message = err.message || "Internal Server Error";
+    err.statusCode = err.statusCode || 500;
+
+    return res.status(err.statusCode).json({
+        success: false,
+        message: err.message
+    });
 }
 
-const errormiddleware =  (err,req,res,next)=>{
-
-    err.message = err.message || "internal server error"
-    err.statusCode= err.statusCode || 500;
-    return res.status(404).json({
-     success: false,
-     message: err.message
-    })
- }
-
-module.exports = {errormiddleware}
+module.exports = { errormiddleware };
